366e87dabfc49e42343ee16ecb0bcdbbfc167e78
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-13 21:19:49 +0530

    Band Generator Program!

---
+++ python/Day 1.py
@@ -0,0 +1,6 @@
+"""This Code is a band Generator using City & Pet Name"""
+
+cityName = input("Enter the Name of the City!\n")
+PetName = input("Enter the Name of your Pet!\n")
+
+print("Your Band Name Could be '{} {}'".format(cityName, PetName))
