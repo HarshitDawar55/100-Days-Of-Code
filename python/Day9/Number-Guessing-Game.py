66bfb9ad0c69bc8337a11adf2d2f02d9cefdcf19
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-28 01:21:49 +0530

    Main Function Done

--- python/Day9/Number-Guessing-Game.py
+++ python/Day9/Number-Guessing-Game.py
@@ -1,3 +1,5 @@
+import random
+
 logo = """
  _   _                 _                 _____                     _               _____                      
 | \ | |               | |               |  __ \                   (_)             |  __ \                     
@@ -8,3 +10,39 @@ logo = """
                                                                             __/ |                             
                                                                            |___/                              
 """
+
+
+def executeGame():
+    print("Welcome to the Number Guessing Game! ")
+    level = int(input("Enter playing Level '1' for Hard & '2' for easy!"))
+    if level == 1:
+        number = random.randint(1, 350)
+        chancesleft = 15
+        while chancesleft > 0:
+            n = int(input("Guess a number: "))
+            if number == n:
+                print(f"Number is {number}, You Won!")
+                break
+            elif number > n:
+                print("You have guessed a bigger number!")
+                chancesleft -=1
+            else:
+                print("You have guessed a smaller number!")
+                chancesleft -= 1
+    elif level == 2:
+        number = random.randint(1, 150)
+        chancesleft = 15
+        while chancesleft > 0:
+            n = int(input("Guess a number: "))
+            if number == n:
+                print(f"Number is {number}, You Won!")
+                break
+            elif number > n:
+                print("You have guessed a bigger number!")
+                chancesleft -= 1
+            else:
+                print("You have guessed a smaller number!")
+                chancesleft -= 1
+
+    else:
+        print("Wrong Choice! ")
